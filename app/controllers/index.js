efd538e45119af2f45f041f557caeb2b208dc29a
Jong Eun Lee <yomybaby@yomybaby.com>
2014-12-28 13:43:45 +0900

    q 를 이용하여 promise 방식으로 변경

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,22 +1,88 @@
 var moment = require('alloy/moment');
+var Q = require('q');
+
 function doClick(e) {
-    updateWeather();
+    updateAllInformation();
 }
 
 $.index.open();
 
-function encodeData(obj, url) {
-	var str = [];
-	for (var p in obj) {
-		str.push(Ti.Network.encodeURIComponent(p) + "=" + Ti.Network.encodeURIComponent(obj[p]));
+
+var httpRequest = (function(){
+	function encodeData(obj, url) {
+		var str = [];
+		for (var p in obj) {
+			str.push(Ti.Network.encodeURIComponent(p) + "=" + Ti.Network.encodeURIComponent(obj[p]));
+		}
+	
+		if (_.indexOf(url, "?") == -1) {
+			return url + "?" + str.join("&");
+		} else {
+			return url + "&" + str.join("&");
+		}
 	}
 
-	if (_.indexOf(url, "?") == -1) {
-		return url + "?" + str.join("&");
-	} else {
-		return url + "&" + str.join("&");
-	}
-}
+	return function(args) {
+		var defer, tiHttpClient;
+		var url = args.url;
+		var data = args.data;
+		var method = args.method || 'GET';
+		var headers = args.headers;
+		
+		defer = Q.defer();
+	
+	
+		tiHttpClient = Ti.Network.createHTTPClient({
+			onload: function() {
+				var parsedData, error;
+				if (this.status >= 200 && this.status < 300) {
+					try { parsedData = JSON.parse(this.responseText); }
+					catch (e) { error = e; }
+				}
+				else {
+					error = "Bad HTTP Code";
+				}
+				if (error) {
+					defer.reject({
+						status:  this.status,
+						message: error
+					});
+				}
+				else {
+					defer.resolve({
+						status:  this.status,
+						data: parsedData
+					});
+				}
+			},
+			onerror: function() {
+				defer.reject({
+					status:  this.status,
+					message: this.responseText
+				});
+			},
+			onsendstream: function(e) {
+				defer.notify(e);
+			},
+			timeout: 1000
+		});
+	
+	
+		if(method == 'GET' && data){
+			url = encodeData(data,url);
+		}
+		
+		tiHttpClient.open(method, url, true);
+		
+		_.each(headers,function(value,key){
+			tiHttpClient.setRequestHeader(key,value);
+		});
+		tiHttpClient.send(method == 'POST'?data:null);
+	
+		return defer.promise;
+	};
+})();
+
 
 
 /*
@@ -30,143 +96,133 @@ weatherModifyCode
 weatherModifyDescription
  */
 
-function updateWeather(){
-	//$.label.text = "확인중...";
-	$.updateInfo.text = '';
-	
+
+
+function getCurrentPosition() {
+	var data = "",
+	    defer = Q.defer();
 	Ti.Geolocation.getCurrentPosition(function(e){
-		// Create an HTTPClient.
-		var anXhr = Ti.Network.createHTTPClient();
-		anXhr.setTimeout(10000);
-		
-		// Define the callback.
-		anXhr.onload = function() {
-			// Handle the XML data.
-			var resultObj = JSON.parse(this.responseText);
-			$.modifyDesc.text = resultObj.weatherModifyDescription;
-			$.statusDesc.text = resultObj.weatherStatusDescription;
-			
-			getFlickrPhotos({text : resultObj.weatherModifyDescription, targetProxy : $.modifyDescImage});
-			getFlickrPhotos({text : resultObj.weatherStatusDescription, targetProxy : $.statusImage});
-			
-			$.updateInfo.text = moment().format('llll');
-		};
-		anXhr.onerror = function(e) {
-			alert(e);
-		};
-		
-		// Send the request data.
-		anXhr.open('GET',encodeData({
-			latitude : e.coords.latitude,
-			longitude : e.coords.longitude
-		},'https://apis.sktelecom.com/v1/weather/status'));
-			
-		anXhr.setRequestHeader('TDCProjectKey','2045a1d8-0ff7-46f3-b0e6-ae608ddd25dd');
-		anXhr.setRequestHeader('Accept','application/json');
-		anXhr.send();
-		
-		
-		updateWeatherStatus(e); //via sk planet api
-		
-		
+		if(e.success){
+			defer.resolve(e);
+		}else{
+			defer.reject("GEO ERROR: " + e.code + ', ' +e.error);
+		}
+	});
+	
+	return defer.promise;
+}
+
+function updateAllInformation(){
+	getCurrentPosition().then(function(e){
+		updateWeather(e);
+		updateWeatherStatus(e);
 		updateLandmarkName(e);
 	});
 }
 
+function updateWeather(e){
+	//$.label.text = "확인중...";
+	$.updateInfo.text = '';
+	
+	httpRequest({
+		url : 'https://apis.sktelecom.com/v1/weather/status',
+		method : 'GET',
+		data : {
+			latitude : e.coords.latitude,
+			longitude : e.coords.longitude
+		},
+		headers : {
+			'TDCProjectKey' : '2045a1d8-0ff7-46f3-b0e6-ae608ddd25dd',
+			'Accept' : 'application/json'
+		}
+	}).then(function(result){
+		
+		$.modifyDesc.text = result.data.weatherModifyDescription;
+		$.statusDesc.text = result.data.weatherStatusDescription;
+		
+		getFlickrPhotos({text : result.data.weatherModifyDescription, targetProxy : $.modifyDescImage});
+		getFlickrPhotos({text : result.data.weatherStatusDescription, targetProxy : $.statusImage});
+		
+		$.updateInfo.text = moment().format('llll');
+	});
+}
+
 function updateLandmarkName(e){
-	var xhr = Ti.Network.createHTTPClient();
-	xhr.setTimeout(10000);
-	
-	xhr.onload = function(e) {
-		var resultObj = JSON.parse(this.responseText);
-		
-		var landmarkname = resultObj.results[0].name;
+	httpRequest({
+		url : 'https://apis.sktelecom.com/v1/zonepoi/pois',
+		method : 'GET',
+		data : {
+			latitude : e.coords.latitude,
+			longitude : e.coords.longitude
+		},
+		headers : {
+			'TDCProjectKey' : '2045a1d8-0ff7-46f3-b0e6-ae608ddd25dd',
+			'Accept' : 'application/json'
+		}
+	}).then(function(result){
+		alert(1);
+		var landmarkname = result.data.results[0].name;
 		$.landmark.text = landmarkname;
-		
-		getFlickrPhotos({text : landmarkname.replace(/\((.*)?\)/,''), targetProxy : $.landmarkImage});
-	};
-	
-	xhr.onerror = function(e) {
-		alert(e);
-	};
-	
-	xhr.open('GET',encodeData({
-		latitude : e.coords.latitude,
-		longitude : e.coords.longitude
-	},'https://apis.sktelecom.com/v1/zonepoi/pois'));
-	xhr.setRequestHeader('TDCProjectKey','2045a1d8-0ff7-46f3-b0e6-ae608ddd25dd');
-	xhr.setRequestHeader('Accept','application/json');
-	xhr.send();
+		console.log(result.data);
+		//getFlickrPhotos({text : landmarkname.replace(/\((.*)?\)/,''), targetProxy : $.landmarkImage});
+	});
 }
 
 function updateWeatherStatus(e){
-	// Create an HTTPClient.
-	var planetXhr = Ti.Network.createHTTPClient();
-	planetXhr.setTimeout(10000);
-	
-	// Define the callback.
-	planetXhr.onload = function() {
-		// Handle the XML data.
-		var resultObj = JSON.parse(this.responseText);
-		var mData = resultObj.weather.minutely[0];
+	// ref :  https://developers.skplanetx.com/apidoc/kor/weather/information/#doc1168
+	httpRequest({
+		url : 'http://apis.skplanetx.com/weather/current/minutely',
+		method : 'GET',
+		data : {
+			version : '1',
+			lat : e.coords.latitude,
+			lon : e.coords.longitude
+		},
+		headers : {
+			'appKey' : 'ea5b6542-e779-3fad-836d-a8e760539fe3',
+			'Accept' : 'application/json'
+		}
+	}).then(function(result){
+		var mData = result.data.weather.minutely[0];
 		var precipitationTexts = ["없음", "비", "비/눈", "눈"];
 		
-		$.status.text = String.format("바람 : %sms \n강수 : %s, \n기온 : %s도 ( %s ~ %s ) \n습도 : %s%%",
-			mData.wind.wspd,
-			precipitationTexts[parseInt(mData.precipitation.type)],
-			mData.temperature.tc,
-			mData.temperature.tmin,
-			mData.temperature.tmax,
-			mData.humidity
-		);
-	};
-	planetXhr.onerror = function(e) {
-		alert(e);
-	};
-	
-	// Send the request data.
-	planetXhr.open('GET',encodeData({
-		version : '1',
-		lat : e.coords.latitude,
-		lon : e.coords.longitude
-	},'http://apis.skplanetx.com/weather/current/minutely'));
-	planetXhr.setRequestHeader('appKey','ea5b6542-e779-3fad-836d-a8e760539fe3');
-	planetXhr.setRequestHeader('Accept','application/json');
-	planetXhr.send();
+		$.status.text = JSON.stringify(mData,null, '  ');
+		// $.status.text = String.format("바람 : %sms \n강수 : %s, \n기온 : %s도 ( %s ~ %s ) \n습도 : %s%%",
+			// mData.wind.wspd,
+			// precipitationTexts[parseInt(mData.precipitation.type)],
+			// mData.temperature.tc,
+			// mData.temperature.tmin,
+			// mData.temperature.tmax,
+			// mData.humidity
+		// );
+	});
 }
 
-updateWeather();
-Ti.App.addEventListener('resume', function(e) {
-	updateWeather();
-});
-
+updateAllInformation();
+Ti.App.addEventListener('resume', updateAllInformation);
 
 function getFlickrPhotos(args){
 	args = args || {};
 	
-	var xhr = Ti.Network.createHTTPClient();
-	xhr.setTimeout(10000);
-	
-	xhr.onload = function(e) {
-		var resultObj = JSON.parse(this.responseText);
-		var photo = resultObj.photos.photo[_.random(0,resultObj.photos.photo.length)];
+	httpRequest({
+		url : 'https://api.flickr.com/services/rest/',
+		method : 'GET',
+		data : {
+			method : 'flickr.photos.search',
+			api_key : '3daf938f07bf4146d6a416260a5737b1',
+			text : args.text,
+			format : 'json',
+			nojsoncallback : 1
+		},
+		headers : {
+			'Accept' : 'application/json'
+		}
+	}).then(function(result){
+		var photo = result.data.photos.photo[_.random(0,result.data.photos.photo.length)];
 		
-		args.targetProxy && (args.targetProxy.image = 'http://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_n.jpg');
-		// alert($.statusImage.image);
-	};
-	
-	xhr.onerror = function(e) {
-		alert(e);
-	};
-	
-	xhr.open('GET',encodeData({
-		method : 'flickr.photos.search',
-		api_key : '3daf938f07bf4146d6a416260a5737b1',
-		text : args.text,
-		format : 'json',
-		nojsoncallback : 1
-	},'https://api.flickr.com/services/rest/'));
-	xhr.setRequestHeader('TDCProjectKey','2045a1d8-0ff7-46f3-b0e6-ae608ddd25dd');
-	xhr.setRequestHeader('Accept','application/json');
-	xhr.send();
+		if(photo && photo.farm)
+		{
+			args.targetProxy && (args.targetProxy.image = 'http://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_n.jpg');
+		}
+	});
 }
